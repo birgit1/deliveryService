b0ca2e5af31e8c78e8775b4dd4c0651c34ffd461
birgit1 <birgit1@users.noreply.github.com>
2017-10-27 23:40:17 -0600

    base
    base without restaurants

--- js/app.js
+++ js/app.js
@@ -2,12 +2,22 @@
 var app = angular.module('MyApp', ['pascalprecht.translate'])
     .config(function ($translateProvider) {
         $translateProvider.translations('en', {
-            TITLE: 'delivery service',
-            TEXT: 'lalaal test this is english'
+            TITLE1: 'delivering Canmore and area',
+            TITLE2: 'call phone#',
+            RESTAURANTS1: 'Our restaurants',
+            HIW1: 'How it works',
+            HIW2: 'You give us a call and we pick up your order at the restaurant and deliver it to you for only 6$ for the whole delivery. You can pay at your doorstep with cash, credit or debit.',
+            CONT1: 'Contacts',
+            CONT2: 'Call'
         });
-        $translateProvider.translations('de', {
-            TITLE: 'Lieferservice',
-            TEXT: 'und deutsch'
+        $translateProvider.translations('fr', {
+            TITLE1: 'FR',
+            TITLE2: 'FR',
+            RESTAURANTS1: 'Restaurants',
+            HIW1: 'FR',
+            HIW2: 'FR',
+            CONT1: 'FR',
+            CONT2: 'FR'
         });
 
         $translateProvider.preferredLanguage('en');
