43529406608c45a2f7931d2111b22a9197b85a4d
birgit1 <birgit1@users.noreply.github.com>
2017-10-28 23:14:45 -0600

    working frontend basics
    working frontend home page

--- js/app.js
+++ js/app.js
@@ -8,7 +8,8 @@ var app = angular.module('MyApp', ['pascalprecht.translate'])
             HIW1: 'How it works',
             HIW2: 'You give us a call and we pick up your order at the restaurant and deliver it to you for only 6$ for the whole delivery. You can pay at your doorstep with cash, credit or debit.',
             CONT1: 'Contacts',
-            CONT2: 'Call'
+            CONT2: 'Give us a call on ',
+            CONT3: 'or send us an email '
         });
         $translateProvider.translations('fr', {
             TITLE1: 'FR',
@@ -17,7 +18,8 @@ var app = angular.module('MyApp', ['pascalprecht.translate'])
             HIW1: 'FR',
             HIW2: 'FR',
             CONT1: 'FR',
-            CONT2: 'FR'
+            CONT2: 'FR',
+            CONT3: 'FR'
         });
 
         $translateProvider.preferredLanguage('en');
