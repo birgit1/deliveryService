4a30126c090592eeea19a01e7b0d4797b2d2ad52
birgit1 <birgit1@users.noreply.github.com>
2018-01-24 20:28:24 -0700

    design
    design selection

--- js/controller/searchController.js
+++ js/controller/searchController.js
@@ -19,19 +19,40 @@ var controller = angular.module('SearchController', [])
             $scope.restaurants = response.statusText;
         });
 
-        // load restaurants from server request
+        // load foods from server request
         $http({
             method : "GET",
             url : $scope.path+"/foodCategory/getFoodCategories",
         }).then(function onSuccess(response) {
             $scope.foods = response.data;
-            console.log(response);
+            for(var i=0; i<$scope.foods.length; i++)
+            {
+                $scope.foods[i].inSelection = false;
+            }
+            console.log("foods");
+            console.log($scope.foods);
         }, function onError(response) {
             $scope.foods = response.statusText;
+            
         });
 
         $scope.selectFood = function(id)
         {
-            
+            var inVal = $scope.foods[id].inSelection;
+            $scope.foods[id].inSelection = !inVal;
+            console.log(inVal+" -> "+$scope.foods[id].inSelection);
+        }
+
+        $scope.searchFood = function()
+        {
+            var foodStyles = [];
+            for(var i=0; i<$scope.foods.length; i++)
+            {
+                if($scope.foods[i].inSelection)
+                    foodStyles.push($scope.foods[i]._id);
+                 console.log(foodStyles);
+                 
+                 // server request -> restaurant with food styles
+            }
         }
 });
